fce62e23d026a6ca4e37b2d2573f3761401eaa93
hanyilee <hanyilee@naver.com>
2023-10-14 23:10:14 +0900

    initial

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "Str.h"
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main(void)
+{
+	Str a("I'm a girl");
+	cout<<a.contents();
+	a="I'm a boy\n";
+	cout<<a.contents();
+	cout<<a.compare("I'm a a")<<endl;
+	Str b("I'm a b");
+	cout<<b.contents()<<endl;
+	cout<<b.compare(a)<<endl;
+	return 0;
+}
